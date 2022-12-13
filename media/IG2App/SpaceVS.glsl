93f7fc830ab264002390ffa276470bf577db7152
dcasiano <82372508+dcasiano@users.noreply.github.com>
2022-12-13 17:31:57 +0100

    8

--- media/IG2App/SpaceVS.glsl
+++ media/IG2App/SpaceVS.glsl
@@ -2,11 +2,12 @@
 in vec4 vertex; 
 in vec2 uv0; 
 uniform mat4 modelViewProjMat; 
-uniform float ZF;
+uniform float st;
 out vec2 vUv0;
 out vec2 vUv1;
 void main() {
-    vUv0 = uv0;     
+    vUv0 = uv0; 
+    float ZF=st*0.25+0.75;  
     vUv1 = (vUv0-0.5)*ZF+0.5;
     gl_Position = modelViewProjMat * vertex; 
 }
