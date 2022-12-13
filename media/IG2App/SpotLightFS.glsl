305ddcf528d7464b92824c0562a618e7a0a95731
Dewei Chen <deweiche@ucm.es>
2022-12-13 20:44:21 +0100

    13

--- media/IG2App/SpotLightFS.glsl
+++ media/IG2App/SpotLightFS.glsl
@@ -1,25 +1,17 @@
 #version 330 core
 uniform sampler2D textureM;  
-uniform vec4 lightPosition;
-uniform mat4 normalMat;
+
+
 uniform float BF; 
 uniform float intLuzAmb; 
 uniform float cutoff;
 in vec2 vUv0; 
 in vec3 mNormal;
-in vec3 viewVertex;
 out vec4 fFragColor; 
 
-float diff(vec3 cVertex, vec3 cNormal){
-    vec3 lightDir = lightPosition.xyz;
-    if(lightPosition.w == 1)
-        lightDir = lightPosition.xyz - cVertex;
-    return dot(cNormal, normalize(lightDir));
-}
-
 void main() {
-    vec3 viewNormal = normalize(vec3(normalMat * vec4(mNormal,0)));
-    float diffuse = diff(lightPosition.xyz, viewNormal);
+    vec3 viewNormal = normalize(vec3(mNormal));
+    float diffuse = dot(viewNormal,normalize(vec3(0,1,0)));
     vec3 colorM =  texture(textureM,vUv0).rgb;
     vec3 color = colorM;
     if(diffuse < cutoff+1){
--- media/IG2App/SpotLightVS.glsl
+++ media/IG2App/SpotLightVS.glsl
@@ -6,9 +6,7 @@ uniform mat4 modelViewProjMat;
 uniform mat4 modelViewMat;
 out vec2 vUv0;
 out vec3 mNormal;
-out vec3 viewVertex;
 void main() {
-    viewVertex = vec3(modelViewMat * vertex);
     mNormal = normal;
     vUv0 = uv0; 
     gl_Position = modelViewProjMat * vertex; 
